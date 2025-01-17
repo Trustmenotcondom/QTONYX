7364ab9aceb25a0388b74cb8e1ce97b21c4e3eb8
Vin <xyxypogi6@gmail.com>
2025-01-17 23:11:06 +0800

    Update Debug.lua

--- Debug.lua
+++ Debug.lua
@@ -1,5 +1,6 @@
 task.spawn(function()
     hookfunction(require(game:GetService("ReplicatedStorage"):FindFirstChild("Effect"):FindFirstChild("Container"):FindFirstChild("Death")), function(...) return ... end)
+    hookfunction(require(game:GetService("ReplicatedStorage"):FindFirstChild("Effect"):FindFirstChild("Container"):FindFirstChild("Respawn")), function(...) return ... end)
     hookfunction(require(game:GetService("ReplicatedStorage"):FindFirstChild("GuideModule")).ChangeDisplayedNPC, function(...) return ... end)
     require(game:GetService("ReplicatedStorage").Util:FindFirstChild("CameraShaker")):Stop()
 end)
