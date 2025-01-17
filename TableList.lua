7f380c85304a4333e613e8212455cbfacb187f3b
Vin <xyxypogi6@gmail.com>
2025-01-17 23:10:39 +0800

    Create TableList.lua

---
+++ TableList.lua
@@ -0,0 +1,6 @@
+getgenv().GamesTables = {
+    [2753915549] = "https://raw.githubusercontent.com/FlazhGG/QTONYX/refs/heads/main/BloxFruit.lua",
+    [4442272183] = "https://raw.githubusercontent.com/FlazhGG/QTONYX/refs/heads/main/BloxFruit.lua",
+    [7449423635] = "https://raw.githubusercontent.com/FlazhGG/QTONYX/refs/heads/main/BloxFruit.lua",
+    [16732694052] = "https://raw.githubusercontent.com/FlazhGG/QTONYX/refs/heads/main/Fisch.lua",
+}
