b3afab25b1c79bf9c39325df60c1bfd150bc854c
Vin <xyxypogi6@gmail.com>
2025-01-29 08:48:23 +0800

    Update Debug.lua

--- Util/Debug.lua
+++ Util/Debug.lua
@@ -3,4 +3,19 @@ task.spawn(function()
     hookfunction(require(game:GetService("ReplicatedStorage"):FindFirstChild("Effect"):FindFirstChild("Container"):FindFirstChild("Respawn")), function(...) return ... end)
     hookfunction(require(game:GetService("ReplicatedStorage"):FindFirstChild("GuideModule")).ChangeDisplayedNPC, function(...) return ... end)
     require(game:GetService("ReplicatedStorage").Util:FindFirstChild("CameraShaker")):Stop()
+    require(game:GetService("ReplicatedStorage").Modules.CombatUtil).CanCharacterMeleeAoe = newcclosure(function()
+        return true
+    end)
+    require(game:GetService("ReplicatedStorage").Modules.CombatUtil).GetComboPaddingTime = newcclosure(function()
+        return 0
+    end)
+    require(game:GetService("ReplicatedStorage").Modules.CombatUtil).GetDefaultAOEDelay = newcclosure(function()
+        return 0
+    end)
+    require(game:GetService("ReplicatedStorage").Modules.CombatUtil).GetAttackCancelMultiplier = newcclosure(function()
+        return 0
+    end)
+    require(game:GetService("ReplicatedStorage").Modules.CombatUtil).CanAttack = newcclosure(function()
+        return true
+    end)
 end)
