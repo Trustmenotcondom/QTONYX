8b5391f9c43eac39c19a8d1f7f87c20f72e042d9
Vin <xyxypogi6@gmail.com>
2025-02-01 22:23:20 +0800

    Delete TableList.lua

--- TableList.lua
+++
@@ -1,6 +0,0 @@
-getgenv().GamesTables = {
-    [2753915549] = "https://raw.githubusercontent.com/Trustmenotcondom/QTONYX/refs/heads/main/BloxFruit.lua",
-    [4442272183] = "https://raw.githubusercontent.com/Trustmenotcondom/QTONYX/refs/heads/main/BloxFruit.lua",
-    [7449423635] = "https://raw.githubusercontent.com/Trustmenotcondom/QTONYX/refs/heads/main/BloxFruit.lua",
-    [16732694052] = "https://raw.githubusercontent.com/Trustmenotcondom/QTONYX/refs/heads/main/Fisch.lua",
-}
