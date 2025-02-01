398e7f429610c036a120d9227288405d6723fc76
Vin <xyxypogi6@gmail.com>
2025-02-01 22:24:53 +0800

    Update QuantumOnyx.lua

--- QuantumOnyx.lua
+++ QuantumOnyx.lua
@@ -5,14 +5,15 @@ getgenv().GamesTables = (function()
         [7449423635] = "https://raw.githubusercontent.com/Trustmenotcondom/QTONYX/refs/heads/main/BloxFruit.lua",
         [16732694052] = "https://raw.githubusercontent.com/Trustmenotcondom/QTONYX/refs/heads/main/Fisch.lua",
     }
-    local function Table(tbl)
+    local function ReverseTable(tbl)
         local proxy = {}
         for key, value in pairs(tbl) do
             proxy[key * 3 - 1] = value:reverse()
         end
         return proxy
     end
-    return Table(GameList)
+
+    return ReverseTable(GameList)
 end)()
 
 local Games = (function(tbl)
@@ -23,18 +24,20 @@ local Games = (function(tbl)
     return Lookup
 end)(getgenv().GamesTables)
 
+local function FetchScript(url)
+    local success, result = pcall(game.HttpGet, game, url)
+    return success and result or nil
+end
+
 local function LoadGameScript(placeId)
     local URL = Games[placeId]
     if not URL then return end
-    local function FetchScript(url)
-        local Success, Result = pcall(game.HttpGet, game, url)
-        return Success and Result or nil
-    end
+
     local Script = FetchScript(URL)
     if Script then
-        local Execute = loadstring(Script)
-        if Execute then
-            Execute()
+        local execute = loadstring(Script)
+        if execute then
+            execute()
         end
     end
 end
