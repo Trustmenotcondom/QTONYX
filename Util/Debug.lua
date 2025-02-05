491cc57ac096601143e6ea2ee8fe9c5d3b970624
Vin <xyxypogi6@gmail.com>
2025-02-05 17:42:17 +0800

    Update Debug.lua

--- Util/Debug.lua
+++ Util/Debug.lua
@@ -1,21 +1,36 @@
 task.spawn(function()
-    hookfunction(require(game:GetService("ReplicatedStorage"):FindFirstChild("Effect"):FindFirstChild("Container"):FindFirstChild("Death")), function(...) return ... end)
-    hookfunction(require(game:GetService("ReplicatedStorage"):FindFirstChild("Effect"):FindFirstChild("Container"):FindFirstChild("Respawn")), function(...) return ... end)
-    hookfunction(require(game:GetService("ReplicatedStorage"):FindFirstChild("GuideModule")).ChangeDisplayedNPC, function(...) return ... end)
-    require(game:GetService("ReplicatedStorage").Util:FindFirstChild("CameraShaker")):Stop()
-    require(game:GetService("ReplicatedStorage").Modules.CombatUtil).CanCharacterMeleeAoe = newcclosure(function()
-        return true
-    end)
-    require(game:GetService("ReplicatedStorage").Modules.CombatUtil).GetComboPaddingTime = newcclosure(function()
-        return 0
-    end)
-    require(game:GetService("ReplicatedStorage").Modules.CombatUtil).GetDefaultAOEDelay = newcclosure(function()
-        return 0
-    end)
-    require(game:GetService("ReplicatedStorage").Modules.CombatUtil).GetAttackCancelMultiplier = newcclosure(function()
-        return 0
-    end)
-    require(game:GetService("ReplicatedStorage").Modules.CombatUtil).CanAttack = newcclosure(function()
-        return true
-    end)
+    local ReplicatedStorage = game:GetService("ReplicatedStorage")
+    local EffectContainer = ReplicatedStorage:FindFirstChild("Effect") and ReplicatedStorage.Effect:FindFirstChild("Container")
+
+    if EffectContainer then
+        local DeathEffect = EffectContainer:FindFirstChild("Death")
+        local RespawnEffect = EffectContainer:FindFirstChild("Respawn")
+
+        if DeathEffect then
+            hookfunction(require(DeathEffect), function(...) return ... end)
+        end
+        if RespawnEffect then
+            hookfunction(require(RespawnEffect), function(...) return ... end)
+        end
+    end
+
+    local GuideModule = ReplicatedStorage:FindFirstChild("GuideModule")
+    if GuideModule then
+        hookfunction(require(GuideModule).ChangeDisplayedNPC, function(...) return ... end)
+    end
+
+    local CameraShaker = ReplicatedStorage:FindFirstChild("Util") and ReplicatedStorage.Util:FindFirstChild("CameraShaker")
+    if CameraShaker then
+        require(CameraShaker):Stop()
+    end
+
+    local CombatUtil = ReplicatedStorage:FindFirstChild("Modules") and ReplicatedStorage.Modules:FindFirstChild("CombatUtil")
+    if CombatUtil then
+        local combat = require(CombatUtil)
+        combat.CanCharacterMeleeAoe = newcclosure(function() return true end)
+        combat.GetComboPaddingTime = newcclosure(function() return 0 end)
+        combat.GetDefaultAOEDelay = newcclosure(function() return 0 end)
+        combat.GetAttackCancelMultiplier = newcclosure(function() return 0 end)
+        combat.CanAttack = newcclosure(function() return true end)
+    end
 end)
