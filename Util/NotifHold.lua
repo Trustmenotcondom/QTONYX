a865301caf0ba1abddad0a8ac20f48a4b9bbee34
Vin <xyxypogi6@gmail.com>
2025-02-08 20:49:42 +0800

    Update NotifHold.lua

--- Util/NotifHold.lua
+++ Util/NotifHold.lua
@@ -7,22 +7,19 @@ local function CreateInstance(class, properties, parent)
     return instance
 end
 
-local GUI = CreateInstance("ScreenGui", {
-    Name = "STX_Nofitication",
-    ZIndexBehavior = Enum.ZIndexBehavior.Sibling,
-},game.CoreGui)
+local GUI = game:GetService("CoreGui"):FindFirstChild("STX_Nofitication")
 
 if not GUI then
-    local STX_Nofitication = CreateInstance("ScreenGui", {
+    GUI = CreateInstance("ScreenGui", {
         Name = "STX_Nofitication",
         ZIndexBehavior = Enum.ZIndexBehavior.Sibling,
         ResetOnSpawn = false
     }, game.CoreGui)
 
-    local STX_NofiticationUIListLayout = CreateInstance("UIListLayout", {
+    CreateInstance("UIListLayout", {
         Name = "STX_NofiticationUIListLayout",
         HorizontalAlignment = Enum.HorizontalAlignment.Right,
         SortOrder = Enum.SortOrder.LayoutOrder,
         VerticalAlignment = Enum.VerticalAlignment.Bottom
-    }, STX_Nofitication)
+    }, GUI)
 end
