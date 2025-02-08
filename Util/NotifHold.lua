85960f94ea80a3175dde5421f9d7e98048969bda
Vin <xyxypogi6@gmail.com>
2025-02-08 20:43:28 +0800

    Update NotifHold.lua

--- Util/NotifHold.lua
+++ Util/NotifHold.lua
@@ -1,16 +1,28 @@
-local GUI = game:GetService("CoreGui"):FindFirstChild("STX_Nofitication")
+local function CreateInstance(class, properties, parent)
+    local instance = Instance.new(class)
+    for key, value in pairs(properties) do
+        instance[key] = value
+    end
+    instance.Parent = parent
+    return instance
+end
+
+local GUI = CreateInstance("ScreenGui", {
+    Name = "STX_Nofitication",
+    ZIndexBehavior = Enum.ZIndexBehavior.Sibling,
+},game.CoreGui)
+
 if not GUI then
-    local STX_Nofitication = Instance.new("ScreenGui")
-    local STX_NofiticationUIListLayout = Instance.new("UIListLayout")
-    STX_Nofitication.Name = "STX_Nofitication"
-    STX_Nofitication.Parent = game.CoreGui
-    STX_Nofitication.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-    STX_Nofitication.ResetOnSpawn = false
-    
-    STX_NofiticationUIListLayout.Name = "STX_NofiticationUIListLayout"
-    STX_NofiticationUIListLayout.Parent = STX_Nofitication
-    STX_NofiticationUIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
-    STX_NofiticationUIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-    STX_NofiticationUIListLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
-else
+    local STX_Nofitication = CreateInstance("ScreenGui", {
+        Name = "STX_Nofitication",
+        ZIndexBehavior = Enum.ZIndexBehavior.Sibling,
+        ResetOnSpawn = false
+    }, game.CoreGui)
+
+    local STX_NofiticationUIListLayout = CreateInstance("UIListLayout", {
+        Name = "STX_NofiticationUIListLayout",
+        HorizontalAlignment = Enum.HorizontalAlignment.Right,
+        SortOrder = Enum.SortOrder.LayoutOrder,
+        VerticalAlignment = Enum.VerticalAlignment.Bottom
+    }, STX_Nofitication)
 end
