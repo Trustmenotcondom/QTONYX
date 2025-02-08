b427a7a94839db5fe20aaf1a675610b702e2dd4e
Vin <xyxypogi6@gmail.com>
2025-02-08 21:19:44 +0800

    Update NotifyLib.lua

--- Util/NotifyLib.lua
+++ Util/NotifyLib.lua
@@ -44,7 +44,7 @@ function Nofitication:Notify(nofdebug, middledebug, all)
         BackgroundColor3 = Color3.fromRGB(25, 25, 25),
         BorderSizePixel = 0,
         Position = UDim2.new(0, 5, 0, 4),
-        Size = UDim2.new(0, 200, 0, 80),
+        Size = UDim2.new(0, 200, 0, 70),
         ZIndex = 2
     }, Shadow)
 
