cf0e94cea372f39d15b5e6cc018f7558e92e3694
Vin <xyxypogi6@gmail.com>
2025-02-08 20:36:16 +0800

    Update NotifyLib.lua

--- Util/NotifyLib.lua
+++ Util/NotifyLib.lua
@@ -1,3 +1,20 @@
+local GUI = game:GetService("CoreGui"):FindFirstChild("STX_Nofitication")
+if not GUI then
+    local STX_Nofitication = Instance.new("ScreenGui")
+    local STX_NofiticationUIListLayout = Instance.new("UIListLayout")
+    STX_Nofitication.Name = "STX_Nofitication"
+    STX_Nofitication.Parent = game.CoreGui
+    STX_Nofitication.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+    STX_Nofitication.ResetOnSpawn = false
+    
+    STX_NofiticationUIListLayout.Name = "STX_NofiticationUIListLayout"
+    STX_NofiticationUIListLayout.Parent = STX_Nofitication
+    STX_NofiticationUIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
+    STX_NofiticationUIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+    STX_NofiticationUIListLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
+else
+end
+
 local Notification = {}
 
 local function CreateInstance(class, properties, parent)
@@ -144,5 +161,6 @@ function Notification:Notify(nofdebug, middledebug, all)
         end)
     end
 end
+
 
 return Notification
