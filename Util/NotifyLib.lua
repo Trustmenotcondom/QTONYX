602caa17a5170662db066457df2a7b55f63d8a1b
Vin <xyxypogi6@gmail.com>
2025-02-08 21:00:13 +0800

    Update NotifyLib.lua

--- Util/NotifyLib.lua
+++ Util/NotifyLib.lua
@@ -19,177 +19,166 @@ local function ApplyUIStroke(parent, color, transparency, size, mode)
     }, parent)
 end
 
-local GUI = CreateInstance("ScreenGui", {
-    Name = "STX_Nofitication",
-    ZIndexBehavior = Enum.ZIndexBehavior.Sibling,
-}, game.CoreGui)
-
+local GUI = game:GetService("CoreGui"):FindFirstChild("STX_Nofitication")
 function Nofitication:Notify(nofdebug, middledebug, all)
     local SelectedType = string.lower(tostring(middledebug.Type))
-    local ambientShadow = Instance.new("ImageLabel")
-    local Window = Instance.new("Frame")
-    local Outline_A = Instance.new("Frame")
-    local WindowTitle = Instance.new("TextLabel")
-    local WindowDescription = Instance.new("TextLabel")
     
-    ambientShadow.Name = "ambientShadow"
-    ambientShadow.Parent = GUI
-    ambientShadow.AnchorPoint = Vector2.new(0.5, 0.5)
-    ambientShadow.BackgroundTransparency = 1.000
-    ambientShadow.BorderSizePixel = 0
-    ambientShadow.Position = UDim2.new(0.91525954, 0, 0.936809778, 0)
-    ambientShadow.Size = UDim2.new(0, 0, 0, 0)
-    ambientShadow.Image = "rbxassetid://1316045217"
-    ambientShadow.ImageColor3 = Color3.fromRGB(0, 0, 0)
-    ambientShadow.ImageTransparency = 0.400
-    ambientShadow.ScaleType = Enum.ScaleType.Slice
-    ambientShadow.SliceCenter = Rect.new(10, 10, 118, 118)
+    local Shadow = CreateInstance("ImageLabel", {
+        AnchorPoint = Vector2.new(0.5, 0.5),
+        BackgroundTransparency = 1,
+        BorderSizePixel = 0,
+        Position = UDim2.new(0.91525954, 0, 0.936809778, 0),
+        Size = UDim2.new(0, 0, 0, 0),
+        Image = "rbxassetid://1316045217",
+        ImageColor3 = Color3.fromRGB(0, 0, 0),
+        ImageTransparency = 0.400,
+        ScaleType = Enum.ScaleType.Slice,
+        SliceCenter = Rect.new(10, 10, 118, 118)
+    }, GUI)
+
+    local Window = CreateInstance("Frame", {
+        Name = "Window",
+        BackgroundColor3 = Color3.fromRGB(25, 25, 25),
+        BorderSizePixel = 0,
+        Position = UDim2.new(0, 5, 0, 5),
+        Size = UDim2.new(0, 230, 0, 80),
+        ZIndex = 2
+    }, Shadow)
+
+    local Outline_A = CreateInstance("Frame", {
+        Name = "Outline_A",
+        BackgroundColor3 = middledebug.OutlineColor,
+        BorderSizePixel = 0,
+        Position = UDim2.new(0, 0, 0, 25),
+        Size = UDim2.new(0, 230, 0, 2),
+        ZIndex = 5
+    }, Window)
     
-    Window.Name = "Window"
-    Window.Parent = ambientShadow
-    Window.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
-    Window.BorderSizePixel = 0
-    Window.Position = UDim2.new(0, 5, 0, 5)
-    Window.Size = UDim2.new(0, 230, 0, 80)
-    Window.ZIndex = 2
+    local WindowTitle = CreateInstance("TextLabel", {
+        Name = "WindowTitle",
+        BackgroundTransparency = 1,
+        BorderSizePixel = 0,
+        Position = UDim2.new(0, 8, 0, 2),
+        Size = UDim2.new(0, 222, 0, 22),
+        ZIndex = 4,
+        Font = Enum.Font.GothamSemibold,
+        Text = nofdebug.Title,
+        TextColor3 = Color3.fromRGB(220, 220, 220),
+        TextSize = 12,
+        TextXAlignment = Enum.TextXAlignment.Left
+    }, Window)
+
+    CreateInstance("TextLabel", {
+        Name = "WindowDescription",
+        BackgroundTransparency = 1,
+        BorderSizePixel = 0,
+        Position = UDim2.new(0, 8, 0, 34),
+        Size = UDim2.new(0, 216, 0, 40),
+        ZIndex = 4,
+        Font = Enum.Font.GothamSemibold,
+        Text = nofdebug.Description,
+        TextColor3 = Color3.fromRGB(180, 180, 180),
+        TextSize = 12,
+        TextWrapped = true,
+        TextXAlignment = Enum.TextXAlignment.Left,
+        TextYAlignment = Enum.TextYAlignment.Top
+    }, Window)
     
-    Outline_A.Name = "Outline_A"
-    Outline_A.Parent = Window
-    Outline_A.BackgroundColor3 = middledebug.OutlineColor
-    Outline_A.BorderSizePixel = 0
-    Outline_A.Position = UDim2.new(0, 0, 0, 25)
-    Outline_A.Size = UDim2.new(0, 230, 0, 2)
-    Outline_A.ZIndex = 5
-    
-    WindowTitle.Name = "WindowTitle"
-    WindowTitle.Parent = Window
-    WindowTitle.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    WindowTitle.BackgroundTransparency = 1.000
-    WindowTitle.BorderColor3 = Color3.fromRGB(27, 42, 53)
-    WindowTitle.BorderSizePixel = 0
-    WindowTitle.Position = UDim2.new(0, 8, 0, 2)
-    WindowTitle.Size = UDim2.new(0, 222, 0, 22)
-    WindowTitle.ZIndex = 4
-    WindowTitle.Font = Enum.Font.GothamSemibold
-    WindowTitle.Text = nofdebug.Title
-    WindowTitle.TextColor3 = Color3.fromRGB(220, 220, 220)
-    WindowTitle.TextSize = 12.000
-    WindowTitle.TextXAlignment = Enum.TextXAlignment.Left
-    
-    WindowDescription.Name = "WindowDescription"
-    WindowDescription.Parent = Window
-    WindowDescription.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    WindowDescription.BackgroundTransparency = 1.000
-    WindowDescription.BorderColor3 = Color3.fromRGB(27, 42, 53)
-    WindowDescription.BorderSizePixel = 0
-    WindowDescription.Position = UDim2.new(0, 8, 0, 34)
-    WindowDescription.Size = UDim2.new(0, 216, 0, 40)
-    WindowDescription.ZIndex = 4
-    WindowDescription.Font = Enum.Font.GothamSemibold
-    WindowDescription.Text = nofdebug.Description
-    WindowDescription.TextColor3 = Color3.fromRGB(180, 180, 180)
-    WindowDescription.TextSize = 12.000
-    WindowDescription.TextWrapped = true
-    WindowDescription.TextXAlignment = Enum.TextXAlignment.Left
-    WindowDescription.TextYAlignment = Enum.TextYAlignment.Top
 
     if SelectedType == "default" then
         local function ORBHB_fake_script()
-            local script = Instance.new('LocalScript', ambientShadow)
+            local script = Instance.new('LocalScript', Shadow)
         
-            ambientShadow:TweenSize(UDim2.new(0, 240, 0, 90), "Out", "Linear", 0.2)
+            Shadow:TweenSize(UDim2.new(0, 240, 0, 90), "Out", "Linear", 0.2)
             Window.Size = UDim2.new(0, 230, 0, 80)
             Outline_A:TweenSize(UDim2.new(0, 0, 0, 2), "Out", "Linear", middledebug.Time)
     
             wait(middledebug.Time)
         
-            ambientShadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
+            Shadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
             
             wait(0.2)
-            ambientShadow:Destroy()
+            Shadow:Destroy()
         end
         coroutine.wrap(ORBHB_fake_script)()
     elseif SelectedType == "image" then
-        ambientShadow:TweenSize(UDim2.new(0, 240, 0, 90), "Out", "Linear", 0.2)
+        Shadow:TweenSize(UDim2.new(0, 240, 0, 90), "Out", "Linear", 0.2)
         Window.Size = UDim2.new(0, 230, 0, 80)
         WindowTitle.Position = UDim2.new(0, 24, 0, 2)
-        local ImageButton = Instance.new("ImageButton")
-        ImageButton.Parent = Window
-        ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-        ImageButton.BackgroundTransparency = 1.000
-        ImageButton.BorderSizePixel = 0
-        ImageButton.Position = UDim2.new(0, 4, 0, 4)
-        ImageButton.Size = UDim2.new(0, 18, 0, 18)
-        ImageButton.ZIndex = 5
-        ImageButton.AutoButtonColor = false
-        ImageButton.Image = all.Image
-        ImageButton.ImageColor3 = all.ImageColor
+        CreateInstance("ImageButton", {
+            BackgroundTransparency = 1,
+            BorderSizePixel = 0,
+            Position = UDim2.new(0, 4, 0, 4),
+            Size = UDim2.new(0, 18, 0, 18),
+            ZIndex = 5,
+            AutoButtonColor = false,
+            Image = all.Image,
+            ImageColor3 = all.ImageColor
+        }, Window)
 
         local function ORBHB_fake_script()
-            local script = Instance.new('LocalScript', ambientShadow)
+            local script = Instance.new('LocalScript', Shadow)
         
             Outline_A:TweenSize(UDim2.new(0, 0, 0, 2), "Out", "Linear", middledebug.Time)
 
             wait(middledebug.Time)
         
-            ambientShadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
+            Shadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
             
             wait(0.2)
-            ambientShadow:Destroy()
+            Shadow:Destroy()
         end
         coroutine.wrap(ORBHB_fake_script)()
     elseif SelectedType == "option" then
-        ambientShadow:TweenSize(UDim2.new(0, 240, 0, 110), "Out", "Linear", 0.2)
+        Shadow:TweenSize(UDim2.new(0, 240, 0, 110), "Out", "Linear", 0.2)
         Window.Size = UDim2.new(0, 230, 0, 100)
-        local Uncheck = Instance.new("ImageButton")
-        local Check = Instance.new("ImageButton")
         
-        Uncheck.Name = "Uncheck"
-        Uncheck.Parent = Window
-        Uncheck.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-        Uncheck.BackgroundTransparency = 1.000
-        Uncheck.BorderSizePixel = 0
-        Uncheck.Position = UDim2.new(0, 7, 0, 76)
-        Uncheck.Size = UDim2.new(0, 18, 0, 18)
-        Uncheck.ZIndex = 5
-        Uncheck.AutoButtonColor = false
-        Uncheck.Image = "http://www.roblox.com/asset/?id=6031094678"
-        Uncheck.ImageColor3 = Color3.fromRGB(255, 84, 84)
-        
-        Check.Name = "Check"
-        Check.Parent = Window
-        Check.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-        Check.BackgroundTransparency = 1.000
-        Check.BorderSizePixel = 0
-        Check.Position = UDim2.new(0, 28, 0, 76)
-        Check.Size = UDim2.new(0, 18, 0, 18)
-        Check.ZIndex = 5
-        Check.AutoButtonColor = false
-        Check.Image = "http://www.roblox.com/asset/?id=6031094667"
-        Check.ImageColor3 = Color3.fromRGB(83, 230, 50)
+        local Uncheck = CreateInstance("ImageButton", {
+            Name = "Uncheck",
+            BackgroundTransparency = 1,
+            BorderSizePixel = 0,
+            Position = UDim2.new(0, 7, 0, 76),
+            Size = UDim2.new(0, 18, 0, 18),
+            ZIndex = 5,
+            AutoButtonColor = false,
+            Image = "http://www.roblox.com/asset/?id=6031094678",
+            ImageColor3 = Color3.fromRGB(255, 84, 84)
+        }, Window)
+
+        local Check = CreateInstance("ImageButton", {
+            Name = "Check",
+            BackgroundTransparency = 1.000,
+            BorderSizePixel = 0,
+            Position = UDim2.new(0, 28, 0, 76),
+            Size = UDim2.new(0, 18, 0, 18),
+            ZIndex = 5,
+            AutoButtonColor = false,
+            Image = "http://www.roblox.com/asset/?id=6031094667",
+            ImageColor3 = Color3.fromRGB(83, 230, 50)
+        }, Window)
 
         local function ORBHB_fake_script()
-            local script = Instance.new('LocalScript', ambientShadow)
+            local script = Instance.new('LocalScript', Shadow)
         
             local Stilthere = true
             local function Unchecked()
                 pcall(function()
                     all.Callback(false)
                 end)
-                ambientShadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
+                Shadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
                 
                 wait(0.2)
-                ambientShadow:Destroy()
+                Shadow:Destroy()
                 Stilthere = false
             end
             local function Checked()
                 pcall(function()
                     all.Callback(true)
                 end)
-                ambientShadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
+                Shadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
                 
                 wait(0.2)
-                ambientShadow:Destroy()
+                Shadow:Destroy()
                 Stilthere = false
             end
             Uncheck.MouseButton1Click:Connect(Unchecked)
@@ -201,10 +190,10 @@ function Nofitication:Notify(nofdebug, middledebug, all)
 
             if Stilthere == true then
         
-                ambientShadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
+                Shadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
                 
                 wait(0.2)
-                ambientShadow:Destroy()
+                Shadow:Destroy()
             end
         end
         coroutine.wrap(ORBHB_fake_script)()
